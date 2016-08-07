18d16033704359ec744ca2db83f276c33a9f334f
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2016-08-07 15:22:53 +0200

    Cleanup for Natalie 2. Empty project.

---
+++ Natalie/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Natalie
+//
+//  Created by Marcin Krzyzanowski on 07/08/16.
+//  Copyright © 2016 Marcin Krzyzanowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
