3c0706faab6e7a2d63828a2f0456052bce65a5c0
Marcin Krzyżanowski <marcin.krzyzanowski@gmail.com>
2017-10-17 12:01:24 +0200

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-xcrun swift build -c release --static-swift-stdlib --show-bin-path
+BUILD_PATH=`xcrun swift build -c release --static-swift-stdlib --show-bin-path`
+cp "$BUILD_PATH/natalie" .
